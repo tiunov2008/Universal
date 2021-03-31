f1fa790e07e95b0ee2a2ed5297386b60f1669eb9
tiunov2008 <tiunov2008.va@gmail.ru>
2021-03-31 19:56:29 +0300

    add category page

---
+++ category.php
@@ -0,0 +1,43 @@
+<?php get_header() ?>
+<div class="container">
+    <h1 class="category-title">
+    <?php single_cat_title() ?>
+    </h1>
+    <div class="post-list">
+        <?php if ( have_posts() ){ while ( have_posts() ){ the_post(); ?>
+            <div class="post-card">
+                <img src="<?php echo get_the_post_thumbnail_url(); ?>" alt="" class="post-card-thumb">
+                <div class="post-card-text">
+                    <h2 class="post-card-title"><?php echo mb_strimwidth(get_the_title(), 0 , 20, "..."); ?></h2>
+                    <?php echo mb_strimwidth(get_the_excerpt(), 0 , 100, "...")?>
+                    <div class="author">
+                        <?php $author_id = get_the_author_meta('ID'); ?>
+                        <img src="<?php echo get_avatar_url($author_id);?>" alt="" class="author-avatar" />
+                        <div class="author-info">
+                            <span class="author-name"
+                                ><strong><?php the_author();?></strong></span
+                            >
+                            <span class="date"><?php the_time( 'j F' )?></span>
+                            <div class="comments">
+                                <svg width="19px" height="15px">
+                                    <use xlink:href="<?php echo get_template_directory_uri()?>/assets/img/sprite.svg#comment"></use>
+                                </svg>
+                                <span class="likes-counter"><?php comments_number('0', '1','%');?></span>
+                            </div>
+                            <div class="likes">
+                                <svg width="19px" height="15px">
+                                    <use xlink:href="<?php echo get_template_directory_uri()?>/assets/img/sprite.svg#like"></use>
+                                </svg>
+                                <span class="likes-counter"><?php comments_number('0', '1','%');?></span>
+                            </div>
+                        </div>
+                        <!-- /.author-info -->
+                    </div>
+                </div>
+            </div>
+        <?php } } else { ?>
+            Записей нет.
+        <?php } ?>
+    </div>
+</div>
+<?php get_footer() ?>

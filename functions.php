eca14dc816abe7c6b779d4ce00c77654f6bda8d7
tiunov2008 <tiunov2008.va@gmail.ru>
2021-04-05 09:37:18 +0300

    add Genres and Teachers

--- functions.php
+++ functions.php
@@ -32,6 +32,7 @@ if ( ! function_exists( 'universal_theme_setup' ) ) :
             'header_menu' => 'Меню в шапке',
             'footer_menu' => 'Меню в подвале'
         ] ); 
+
 }
 endif;
 add_action( 'after_setup_theme', 'universal_theme_setup' );
@@ -77,7 +78,61 @@ function register_post_types(){
 		'rewrite'             => true,
 		'query_var'           => true,
 	] );
-}
+	}	
+	// хук, через который подключается функция
+	// регистрирующая новые таксономии (create_lesson_taxonomies)
+	add_action( 'init', 'create_lesson_taxonomies' );
+
+	// функция, создающая 2 новые таксономии "genres" и "writers" для постов типа "lesson"
+	function create_lesson_taxonomies(){
+
+		// Добавляем древовидную таксономию 'genre' (как категории)
+		register_taxonomy('genre', array('lesson'), array(
+			'hierarchical'  => true,
+			'labels'        => array(
+				'name'              => _x( 'Genres', 'taxonomy general name' ),
+				'singular_name'     => _x( 'Genre', 'taxonomy singular name' ),
+				'search_items'      =>  __( 'Search Genres' ),
+				'all_items'         => __( 'All Genres' ),
+				'parent_item'       => __( 'Parent Genre' ),
+				'parent_item_colon' => __( 'Parent Genre:' ),
+				'edit_item'         => __( 'Edit Genre' ),
+				'update_item'       => __( 'Update Genre' ),
+				'add_new_item'      => __( 'Add New Genre' ),
+				'new_item_name'     => __( 'New Genre Name' ),
+				'menu_name'         => __( 'Genre' ),
+			),
+			'show_ui'       => true,
+			'query_var'     => true,
+			'rewrite'       => array( 'slug' => 'the_genre' ),
+		));
+
+		// Добавляем НЕ древовидную таксономию 'Teacher' (как метки)
+		register_taxonomy('Teacher', 'lesson',array(
+			'hierarchical'  => false,
+			'labels'        => array(
+				'name'                        => _x( 'Teachers', 'taxonomy general name' ),
+				'singular_name'               => _x( 'Teacher', 'taxonomy singular name' ),
+				'search_items'                =>  __( 'Search Teachers' ),
+				'popular_items'               => __( 'Popular Teachers' ),
+				'all_items'                   => __( 'All Teachers' ),
+				'parent_item'                 => null,
+				'parent_item_colon'           => null,
+				'edit_item'                   => __( 'Edit Teacher' ),
+				'update_item'                 => __( 'Update Teacher' ),
+				'add_new_item'                => __( 'Add New Teacher' ),
+				'new_item_name'               => __( 'New Teacher Name' ),
+				'separate_items_with_commas'  => __( 'Separate Teachers with commas' ),
+				'add_or_remove_items'         => __( 'Add or remove Teachers' ),
+				'choose_from_most_used'       => __( 'Choose from the most used teachers' ),
+				'menu_name'                   => __( 'teachers' ),
+			),
+			'show_ui'       => true,
+			'query_var'     => true,
+			//'rewrite'       => array( 'slug' => 'the_writer' ), // свой слаг в URL
+		));
+	}
+
 
 add_action( 'widgets_init', 'register_my_widgets' );
 function register_my_widgets(){
